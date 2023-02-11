182d916bbfc8d3014e8f198434d2986bf7c12942
GBarradas <gbarradas1@hotmail.com>
2023-02-11 18:56:13 +0000

    add AA Work 1

---
+++ NaiveBayesUevora.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+
+class NaiveBayesUevora:
+    alpha = float
+
+    #construtor
+
+    def __init__(self,alpha = 0.0 ):
+        self.alpha = alpha
+        self.colunas = list
+        self.numPropriedades = {}      #por coluna
+        self.probaXc = {}           #P(x|c)
+        self.classes = {}           #P(c)
+        self.probaX = {}            #P(x)
+
+        self.xTreino = np.array
+        self.yTreino = np.array
+        self.treinoSize = int       #numero de linhas
+        self.numColunas = int       #numero de colunas -1
+
+    def fit(self, x, y):
+        self.colunas = list(x.columns)
+        self.xTreino = x
+        self.yTreino = y
+        self.treinoSize = x.shape[0]
+        self.numColunas = x.shape[1]
+        for atributo in self.colunas:
+            self.probaXc[atributo] = {}
+            self.probaX[atributo] = {}
+            self.numPropriedades[atributo] = len(np.unique(self.xTreino[atributo]))
+
+            for valorX in np.unique(self.xTreino[atributo]):
+                self.probaX[atributo][valorX] = 0
+                self.probaXc[atributo][valorX] = {}
+
+                for valorY in np.unique(self.yTreino):
+                    self.probaXc[atributo][valorX][valorY] = 0
+                    self.classes[valorY] = 0
+
+        # P(a)
+        for valorY in np.unique(self.yTreino):
+            nOcorrencias = sum(self.yTreino == valorY)
+            #P(c) = ( nOcorrencias + alpha)/(total * (alpha * NPropriedades))
+            self.classes[valorY] = (nOcorrencias + self.alpha) / \
+               (self.treinoSize + (self.alpha * len(np.unique(self.yTreino))))
+
+        # P(b|a)
+        for atributo in self.colunas:
+
+            for valorY in np.unique(self.yTreino):
+                nOcorrY = sum(self.yTreino == valorY)
+                nOcorrX_Y = self.xTreino[atributo][self.yTreino[\
+                    self.yTreino == valorY].index.values.tolist()].value_counts().to_dict()
+                for valor, nOcorr in nOcorrX_Y.items():
+                    self.probaXc[atributo][valor][valorY] = (nOcorr + self.alpha)/ \
+                        (nOcorrY + (self.alpha * self.numPropriedades[atributo]))
+
+
+    def add_property(self,atributo, propriedade):
+        self.probaXc[atributo][propriedade] = {}
+        for valorY in np.unique(self.yTreino):
+            self.probaXc[atributo][propriedade][valorY] = 0
+            nOcorrY = sum(self.yTreino == valorY)
+            nOcorrX_Y = 0
+            self.probaXc[atributo][propriedade][valorY] = (nOcorrX_Y + self.alpha) / \
+                                                        (nOcorrY + (self.alpha * self.numPropriedades[atributo]+1))
+
+
+    def predict(self, x):
+        resultados = []
+        x= np.array(x)
+        for teste in x:
+            possiveisResultados = {}
+            for valorY in np.unique(self.yTreino):
+                Pa = self.classes[valorY]
+                Pba = 1
+                for atributo, propriedade in zip(self.colunas, teste):
+                    if propriedade not in np.unique(self.xTreino[atributo]):
+                        self.add_property(atributo, propriedade)
+                    Pba *= self.probaXc[atributo][propriedade][valorY]
+                possiveisResultados[valorY] = Pba * Pa  # (Pba*Pa)/Pb
+            resultado = max(possiveisResultados,key=lambda x: possiveisResultados[x])
+            resultados.append(resultado)
+        return np.array(resultados)
+
+
+    def accuracy_score(self, x, y):
+        prev = self.predict(x)
+        return round(float((sum(prev == y))/ float(len(y))*100),2)
+
+
+    def precision_score(self, x, y):
+        resultados =[]
+        prev = self.predict(x)
+        for valorY in np.unique(self.yTreino):
+            vp = 0
+            fp = 0
+            for vy , vyp in zip(y, prev):
+                # print(f'{vy},{vyp},{valorY}')
+                if vy == vyp and vyp == valorY:
+                    vp +=1
+                elif vyp != vy and vyp == valorY:
+                    fp +=1
+            if vp + fp == 0:
+                resultados.append(0)
+            else:
+                resultados.append(vp / (vp + fp))
+        return round(float(sum(resultados)/len(resultados)*100),2)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# AA_Work_1
+
+
+## Objetivo:   
+Implementar o algoritmo Naive Bayes para tipos nominais em Python (Python 3) e testar
+no ambiente scikit-learn.
+## Descrição:  
+Pretende-se a implementação de uma classe que permita a utilização do algoritmo Naive
+Bayes com dados do tipo nominal, no ambiente do scikit-learn, com um estimador suavizado
+(smooth estimator), e avaliação do classificador através da exatidão e precisão.
+## Implementação:   
+a classe a implementar -- **NaiveBayesUevora** -- deverá:
+- na inicialização do objeto deve permitir a escolha de um estimador suavizado de acordo
+com:  
+![formula](frml.png)
+
+Em geral este estimador denomina-se “Lindstone smoothing”; no caso particular de 𝛼=1
+teremos o estimador de Laplace; com 𝛼=0 teremos o estimador usual. Este parâmetro
+deverá ser designado alpha do tipo float. O valor de alpha por omissão deverá ser 𝛼=0
+assumindo deste modo um estimador usual.
+- A classe deverá aceitar dados nominais na forma de strings
+- A classe deverá ter um método fit(x,y) para gerar um classificador a partir dum conjunto de
+treino com etiquetas.
+- A classe deverá ter um método predict(x) para, com base no classificador previamente
+definido, gerar predições em função dum conjunto de dados de teste. O Método deve
+devolver uma variável compatível com um array, com dimensões compatíveis com a
+dimensão dos dados de teste usados.
+- A classe deverá ter um método accuracy_score(x,y) para calcular a exatidão (accuracy) dum
+classificador, devolvendo um tipo float, dado um conjunto de teste.
+- A classe deverá ter um método precision_score(x,y) para calcular a precisão (precision) dum
+classificador, devolvendo um tipo float, dado um conjunto de teste. O valor da precisão
+deverá ser a média aritmética da precisão de cada classe, ou seja a precisão considerando
+cada classe como a classe positiva sendo todas as restantes a classe negativa (caso os
+Verdadeiros Positivos mais Falsos Positivos sejam zero, assuma que a precisão será zero).
+Note que poderão existir nos dados de teste valores dos atributos e classe que não ocorrem nos
+dados de treino. Considere esta situação, proponha uma solução, e implemente-a.
+
+## Dados  
+
+Serão disponibilizados no moodle vários conjuntos de ficheiros/dados para testar o trabalho
+
+### Nota : 15 
+[codigo](https://github.com/GBarradas/AA_Work_1.git)
